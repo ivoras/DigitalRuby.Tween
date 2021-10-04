367ff99a0ed72ca0f6a979e4ffa0f0a33f8502f4
Poroh-kun <poroh.kun@gmail.com>
2021-10-05 00:59:43 +0300

    wait for tween

---
+++ WaitForTween.cs
@@ -0,0 +1,16 @@
+﻿using DigitalRuby.Tween;
+
+namespace UnityEngine
+{
+    public class WaitForTween : CustomYieldInstruction
+    {
+        private ITween _tween;
+
+        public WaitForTween(ITween tween)
+        {
+            _tween = tween;
+        }
+
+        public override bool keepWaiting => _tween?.State != TweenState.Stopped;
+    }
+}
